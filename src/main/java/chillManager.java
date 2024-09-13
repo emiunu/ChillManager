573a79e3b23e53f77cc12a55211b82a6cb39d492
Sabrina <s.lopez15@ufromail.cl>
2024-09-12 23:07:40 -0300

    Se crea rama de Sabrina desde rama develop y se crean los primeros dos métodos de la sección "Series".

--- src/main/java/chillManager.java
+++ src/main/java/chillManager.java
@@ -1,3 +1,23 @@
 public class chillManager {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+    }
+
+    //Funciones de series.
+
+    public static Object[][] matrizSeries(){
+        return new Object[10][7];
+    }
+
+    public static Object[][] agregarInfoSerie(Object[][] matrizSeries, String nombre, int temporadas, int capitulos, int temporadaActual, int capituloActual, int calificacion, String comentario){
+        for (int i = 0; i < matrizSeries.length; i++) {
+            matrizSeries[i][0] = nombre;
+            matrizSeries[i][1] = temporadas;
+            matrizSeries[i][2] = capitulos;
+            matrizSeries[i][3] = temporadaActual;
+            matrizSeries[i][4] = capituloActual;
+            matrizSeries[i][5] = calificacion;
+            matrizSeries[i][6] = comentario;
+        }
+        return matrizSeries;
+    }
 }
