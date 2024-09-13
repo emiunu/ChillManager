5aea9d4f76bafd3ccfef8774429281273f58c5c4
David <d.baez03@ufromail.cl>
2024-09-12 23:33:00 -0300

    Se crea la rama david y se agregan las categorias iniciales de los libros

--- src/main/java/chillManager.java
+++ src/main/java/chillManager.java
@@ -1,3 +1,16 @@
+import java.io.ObjectStreamClass;
+
+//Esta es la rama para crear las funciones de Libros.
+
 public class chillManager {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+
+    }
+
+    public static Object[][] categoriasLibros() {
+        Object [][] matrizLibros = new Object[10][6];
+        matrizLibros[0] = new Object[]{(String)"ISBN",(String)"title",(String)"year",(String)"status",(String)"rating",(String)"commment"};
+        return matrizLibros;
+        //Se crea la primera fila con las diferentes categorias.
+    }
 }
