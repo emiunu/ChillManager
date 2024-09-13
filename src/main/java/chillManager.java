7ee511a6495df5a1b1bcd6e8898f6c282e91b059
Martin Carrasco <m.carrasco29@ufromail.cl>
2024-09-12 23:35:52 -0300

    Creación categorías de juegos y método de unicidad.

--- src/main/java/chillManager.java
+++ src/main/java/chillManager.java
@@ -1,3 +1,20 @@
 public class chillManager {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+    }
+
+    public static Object[][] categoriasJuegos () {
+        Object [][] matrizJuegos = new Object[10][6];
+        matrizJuegos[0] = new Object[]{(String)"title",(String)"status",(String)"rating",(String)"comment",(String)"year",(String)"DLC"};
+        return matrizJuegos;
+    }
+
+    public static boolean juegoUnico (Object [][] matrizJuegos, String titulo) {
+        for (Object[] matrizJuego : matrizJuegos) {
+            if (matrizJuego[0] != null) {
+                if (matrizJuego[0].equals(titulo))
+                    return false;
+            }
+        }
+        return true;
+    }
 }
