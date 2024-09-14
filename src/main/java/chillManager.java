68994a28c9db203c9b7a68ffd38537ad358724c8
David <d.baez03@ufromail.cl>
2024-09-13 23:12:34 -0300

    Se agregan las funciones de libro unico para verificar existencia unica de libros y la de agregar libros

--- src/main/java/chillManager.java
+++ src/main/java/chillManager.java
@@ -13,4 +13,38 @@ public class chillManager {
         return matrizLibros;
         //Se crea la primera fila con las diferentes categorias.
     }
+
+
+    public static boolean libroUnico(Object [][]matrizLibros,int ISBN){
+        for (int i = 0; i < matrizLibros.length; i++) {
+            if (matrizLibros[i][0] != null){
+                if (matrizLibros[i][0].equals(ISBN)) {
+                    return false;
+                }
+            }else{
+                break;
+            }
+        }
+        return true;
+        //Verificacion de existencia de libro unico por isbn
+    }
+
+    public static Object[][] agregarLibro(Object[][] matrizLibros,int ISBN,String titulo,String year,String status,String rating,String commment) {
+        if(libroUnico(matrizLibros,ISBN)){
+            for (int i = 0; i < matrizLibros.length; i++) {
+            if (matrizLibros[i][0] == null) {
+                matrizLibros[i][0] = ISBN;
+                matrizLibros[i][1] = titulo;
+                matrizLibros[i][2] = year;
+                matrizLibros[i][3] = status;
+                matrizLibros[i][4] = rating;
+                matrizLibros[i][5] = commment;
+                return matrizLibros;
+            }
+        }
+        }
+        System.out.println("Ya existe este libro en la biblioteca.");
+        return null;
+        //Fucion para agregar un libro nuevo dependiendo de ciertos parametros.
+    }
 }
