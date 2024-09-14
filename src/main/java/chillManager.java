09b74e4809bf3473f571b083959f0982755113af
emiunu <e.jaramillo02@ufromail.cl>
2024-09-13 23:23:36 -0300

    Subiendo funcion para verificar existencia de una pelicula en la matriz, y actualizacion a funcion para añadir pelicula

--- src/main/java/chillManager.java
+++ src/main/java/chillManager.java
@@ -1,4 +1,5 @@
 public class chillManager {
+
     public static void main(String[] args) {}
 
     // Función para crear matriz de pelicula
@@ -7,32 +8,49 @@ public class chillManager {
     }
 
     //Función para añadir valores a la matriz pelicula
-    public static Object[][] annadirPeliculas(Object[][] peliculas, String titulo, int anno, String genero, int duracion, int valoracion, String comentario){
-        for (int i = 0; i < peliculas.length; i++) {
-            if (peliculas[i][0] == null || peliculas[i][0] == "" ) {
-                peliculas[i][0] = titulo;
-                peliculas[i][1] = anno;
-                peliculas[i][2] = genero;
-                peliculas[i][3] = duracion;
-                peliculas[i][4] = valoracion;
-                peliculas[i][5] = comentario;
-                break;
+    public static Object[][] annadirPelicula(Object[][] matrizPeliculas, String titulo, int anno, String genero, int duracion, int valoracion, String comentario){
+        if(peliculaUnica(matrizPeliculas,titulo)){         //Se verifica que el título no esté previamente guardado
+            for (int i = 0; i < matrizPeliculas.length; i++) {
+                if (matrizPeliculas[i][0] == null || matrizPeliculas[i][0] == "" ) {
+                    matrizPeliculas[i][0] = titulo;
+                    matrizPeliculas[i][1] = anno;
+                    matrizPeliculas[i][2] = genero;
+                    matrizPeliculas[i][3] = duracion;
+                    matrizPeliculas[i][4] = valoracion;
+                    matrizPeliculas[i][5] = comentario;
+                    break;
+                }
             }
+            return matrizPeliculas;
+        }else{
+            System.out.println("La pelicula " + titulo + " ya se encuentra guardada");
+            return matrizPeliculas;
         }
-        return peliculas;
+
     }
 
     //Funcion para visualizar datos guardados en la matriz
-    public static void verMatrizPeliculas(Object[][] peliculas){
-        for (int i = 0; i < peliculas.length; i++) {
-            if (peliculas[i][0] == null || peliculas[i][0] == "") {
+    public static void verMatrizPeliculas(Object[][] matrizPeliculas){
+        for (int i = 0; i < matrizPeliculas.length; i++) {
+            if (matrizPeliculas[i][0] == null || matrizPeliculas[i][0] == "") {
                 continue;
             }else{
-                System.out.println("Pelicula: " + peliculas[i][0] + "| Año: " + peliculas[i][1] + "| Género: " + peliculas[i][2] + "| Duración: " + peliculas[i][3] + "| Valoracion: " + peliculas[i][4] + "| Comentario: " + peliculas[i][5]);
+                System.out.println("Pelicula: " + matrizPeliculas[i][0] + "| Año: " + matrizPeliculas[i][1] + "| Género: " + matrizPeliculas[i][2] + "| Duración: " + matrizPeliculas[i][3] + "| Valoracion: " + matrizPeliculas[i][4] + "| Comentario: " + matrizPeliculas[i][5]);
 
             }
         }
     }
 
+    //Función para verificar la unicidad de una pelicula en la matriz
+    public static boolean peliculaUnica(Object[][] matrizPeliculas, String titulo){
+        for (int i = 0; i < matrizPeliculas.length; i++) {
+            if (matrizPeliculas[i][0] != null){
+                if (matrizPeliculas[i][0].equals(titulo)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
 }
