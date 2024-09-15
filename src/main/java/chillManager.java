a47a6cb2b5cb3da35f9040bbf361f0540a85f2c1
Sabrina <s.lopez15@ufromail.cl>
2024-09-15 20:33:04 -0300

    Se finaliza la primera versión completa del código. Nuevas operaciones: se completó lo necesario para agregar series, se implementaron las operaciones buscar serie, actualizar serie, y eliminar serie. Además, se agregaron comentarios para una mejor documentación, se agregaron métodos secundarios para pedir el estado de visualización y para vaciar los datos de la serie, y se ajustaron algunos métodos secundarios como serieUnica(), obtenerFilaSerie() y continuarMenu()

--- src/main/java/chillManager.java
+++ src/main/java/chillManager.java
@@ -14,7 +14,7 @@ public class chillManager {
 
     //Función para crear la matriz de objetos.
     public static Object[][] matrizSeries(){
-        return new Object[10][7];
+        return new Object[10][8];
     }
 
     //Función para pedir un Int, junto con el mensaje correspondiente de para qué se utilizará.
@@ -25,8 +25,8 @@ public class chillManager {
                 System.out.print(mensaje);
                 valor = scanner().nextInt();
                 break;
-            } catch(Exception InputMismatchException){
-                System.out.print("Entrada no válida. Ingrese un número. ");
+            } catch(Exception InputMismatchException) {
+                System.out.println("Entrada no válida. Ingrese un número. ");
             }
         }
         return valor;
@@ -38,30 +38,64 @@ public class chillManager {
         return scanner().nextLine();
     }
 
-    //Función para ingresar un rating entre 1 y 10.
+    //Función para ingresar un rating restringido entre 1 y 10.
     public static int pedirRating(){
         int rating;
         while (true) {
             try {
-                System.out.print("Ingrese su rating (del 1 al 10): ");
+                System.out.print("Ingrese su rating (del 1 al 10 ; 1 = puntaje más bajo ; 10 = puntaje más alto): ");
                 rating = scanner().nextInt();
                 if ((rating >= 1) && (rating <= 10)) {
                     break;
                 } else {
-                    System.out.print("Opción inválida. Inténtelo nuevamente: ");
+                    System.out.println("Opción inválida. Inténtelo nuevamente.");
                 }
-            } catch(Exception InputMismatchException){
-                System.out.print("Entrada no válida. Ingrese un número: ");
+            } catch(Exception InputMismatchException) {
+                System.out.println("Entrada no válida. Ingrese un número.");
             }
         }
         return rating;
     }
 
-    //Función para comprobar que si ya existe. Compara el título en minúsculas y sin espacios.
+    //Función para pedir el estado de visualización.
+    public static int pedirStatus(){
+        int opcion;
+        while (true) {
+            try {
+                System.out.print("Ingrese el estado de visualización (1 = Sin empezar ; 2 = Viendo ; 3 = Terminada): ");
+                opcion = scanner().nextInt();
+                if ((opcion >= 1) && (opcion <= 3)) {
+                    break;
+                } else {
+                    System.out.println("Opción inválida. Inténtelo nuevamente.");
+                }
+            } catch (Exception InputMismatchException) {
+                System.out.println("Entrada no válida. Ingrese un número.");
+            }
+        }
+        return opcion;
+    }
+
+    //Transformar lo pedido en la función pedirStatus().
+    public static String transformarStatus(int opcion){
+        String status = "";
+        if (opcion == 1){
+            status = "Sin empezar";
+        } else if (opcion == 2){
+            status = "Viendo";
+        } else if (opcion == 3){
+            status = "Terminada";
+        }
+        return status;
+    }
+
+    //Función para comprobar que si ya existe. Compara el título con la posición en la matriz, ambas cosas en minúsculas y sin espacios.
     public static boolean serieUnica(Object[][] matrizSeries, String titulo){
+        String tituloIngresado = titulo.replace(" ","").toLowerCase();
         for (int i = 0; i < matrizSeries.length; i++) {
-            if (matrizSeries[i][0] != null){
-                if (matrizSeries[i][0] == titulo) {
+            if (matrizSeries[i][0] != null) {
+                String tituloBuscado = (String)matrizSeries[i][0];
+                if (tituloBuscado.replace(" ","").toLowerCase().equals(tituloIngresado)) {
                     return false;
                 }
             }
@@ -69,17 +103,21 @@ public class chillManager {
         return true;
     }
 
-    //Función para obtener la fila en la que está la serie que se busca. Solo debe ser llamada si la serie fue encontrada (ya existe en la matriz).
+    //Función para obtener la fila en la que está la serie que se busca. Solo debe ser llamada si la serie fue encontrada (ya existe en la matriz). Se compara el título con la posición en la matriz, ambas cosas en minúsculas y sin espacios.
     public static int obtenerFilaDeSerie(Object[][] matrizSeries, String titulo){
+        String tituloIngresado = titulo.replace(" ","").toLowerCase();
         for (int i = 0; i < matrizSeries.length; i++) {
-            if (matrizSeries[i][0].equals(titulo)){
-                return i;
+            if (matrizSeries[i][0] != null) {
+                String tituloBuscado = (String)matrizSeries[i][0];
+                if (tituloBuscado.replace(" ","").toLowerCase().equals(tituloIngresado)) {
+                    return i;
+                }
             }
         }
         return 0;
     }
 
-    //Función para saber si existe espacio disponible.
+    //Función para saber si existe espacio disponible en la matriz.
     public static boolean espacioDisponible(Object[][] matrizSeries){
         for (int i = 0; i < matrizSeries.length; i++) {
             if (matrizSeries[i][0] == null){
@@ -89,7 +127,7 @@ public class chillManager {
         return false;
     }
 
-    //Función para obtener la siguiente fila vacía. Solo debe ser llamada si hay espacio disponible.
+    //Función para obtener la primera fila vacía disponible para agregar datos. Solo debe ser llamada si hay espacio disponible.
     public static int obtenerFilaVacia(Object[][] matrizSeries){
         for (int i = 0; i < matrizSeries.length; i++) {
             if (matrizSeries[i][0] == null){
@@ -99,23 +137,85 @@ public class chillManager {
         return 0;
     }
 
-    //Función para agregar los parámetros a la matriz
-    public static Object[][] agregarInfoSerie(Object[][] matrizSeries, String titulo, int temporadas, int capitulos, int temporadaActual, int capituloActual, int rating, String comentario, int fila){
+    //Función para escribir la información de la serie.
+    public static Object[][] escribirInfoSerie(Object[][] matrizSeries, String titulo, int temporadas, int capitulos, int temporadaActual, int capituloActual, String status, int rating, String comentario, int fila){
         matrizSeries[fila][0] = titulo;
         matrizSeries[fila][1] = temporadas;
         matrizSeries[fila][2] = capitulos;
         matrizSeries[fila][3] = temporadaActual;
         matrizSeries[fila][4] = capituloActual;
-        matrizSeries[fila][5] = rating;
-        matrizSeries[fila][6] = comentario;
+        matrizSeries[fila][5] = status;
+        matrizSeries[fila][6] = rating;
+        matrizSeries[fila][7] = comentario;
         return matrizSeries;
+    }
+
+    //Mini menú para gestionar el agregar una serie.
+    public static void agregarSerie(Object[][] matrizSeries, String titulo){
+        if (serieUnica(matrizSeries,titulo)) { //Si la serie es única.
+            if (espacioDisponible(matrizSeries)) { //Y si queda espacio disponible en la matriz.
+                escribirInfoSerie(matrizSeries, titulo, pedirInt("Ingrese las temporadas totales: "), pedirInt("Ingrese los capítulos totales: "), pedirInt("Ingrese la temporada actual que está viendo: "), pedirInt("Ingrese el capítulo actual (de la temporada) en el que quedó: "), transformarStatus(pedirStatus()), pedirRating(), pedirString("Ingrese un comentario (Escribir un espacio si quiere dejar vacío):"), obtenerFilaVacia(matrizSeries));
+                //la función pedirStatus(), que devuelve un int, se utiliza como parámetro de transformarStatus(...), que pide un int y retorna un String, para escribir correctamente el estado de visualización una de las 3 opciones disponibles.
+            } else { //Si no hay espacio disponible.
+                System.out.println("No queda espacio disponible para agregar series.");
+            }
+        } else { //Si la serie ya se encuentra en la matriz.
+            System.out.println("La serie ya se encuentra registrada.");
+        }
+    }
+
+    //Función para buscar una serie.
+    public static void buscarSerie(Object[][] matrizSeries, String titulo){
+        if (!serieUnica(matrizSeries,titulo)) { //Si la serie ya se encuentra en la matriz.
+            int fila = obtenerFilaDeSerie(matrizSeries,titulo); //Obtener la fila donde está esa serie.
+            System.out.println("Serie encontrada.");
+            System.out.println("『 Título: "+matrizSeries[fila][0]+" | Temporadas: "+matrizSeries[fila][1]+" | Capítulos: "+matrizSeries[fila][2]+" | Temporada Actual: "+matrizSeries[fila][3]+" | Capítulo Actual (de temporada): "+matrizSeries[fila][4]+" | Estado: "+matrizSeries[fila][5]+" | Rating: "+matrizSeries[fila][6]+" | Comentarios: "+matrizSeries[fila][7]+" 』");
+        } else {
+            System.out.println("No se encontró la serie. Asegúrese de ingresar el título correctamente.");
+        }
+    }
+
+    //Función para actualizar una serie.
+    public static void actualizarSerie(Object[][] matrizSeries, String titulo){
+        if (!serieUnica(matrizSeries,titulo)) { //Si la serie ya se encuentra en la matriz.
+            int fila = obtenerFilaDeSerie(matrizSeries,titulo); //Obtener la fila donde está esa serie.
+            escribirInfoSerie(matrizSeries, pedirString("Ingrese el nuevo título: "), pedirInt("Ingrese las temporadas totales: "), pedirInt("Ingrese los capítulos totales: "), pedirInt("Ingrese la temporada actual que está viendo: "), pedirInt("Ingrese el capítulo actual (de la temporada) en el que quedó: "), transformarStatus(pedirStatus()), pedirRating(), pedirString("Ingrese un comentario (Escribir un espacio si quiere dejar vacío):"), fila);
+            //reemplaza la información de la serie en la fila donde ya está.
+        } else {
+            System.out.println("No se encontró la serie. Asegúrese de ingresar el título correctamente.");
+        }
+    }
+
+    //Función para reemplazar todos los valores de la serie por null.
+    public static Object[][] vaciarSerie(Object[][] matrizSeries, int fila){
+        matrizSeries[fila][0] = null;
+        matrizSeries[fila][1] = null;
+        matrizSeries[fila][2] = null;
+        matrizSeries[fila][3] = null;
+        matrizSeries[fila][4] = null;
+        matrizSeries[fila][5] = null;
+        matrizSeries[fila][6] = null;
+        matrizSeries[fila][7] = null;
+        return matrizSeries;
+    }
+
+    //Función para eliminar una serie.
+    public static void eliminarSerie(Object[][] matrizSeries, String titulo){
+        if (!serieUnica(matrizSeries,titulo)) { //Si la serie ya se encuentra en la matriz.
+            int fila = obtenerFilaDeSerie(matrizSeries,titulo); //Obtener la fila donde está esa serie.
+            vaciarSerie(matrizSeries,fila);
+        } else {
+            System.out.println("No se encontró la serie. Asegúrese de ingresar el título correctamente.");
+        }
     }
 
     //Función para mostrar todas las series.
     public static void mostrarSeries(Object[][] matrizSeries){
+        System.out.println("LISTA DE SERIES");
+        System.out.println("===================================");
         for (int i = 0; i < matrizSeries.length; i++) {
            if (matrizSeries[i][0] != null) {
-               System.out.println("Título: "+matrizSeries[i][0]+" | Temporadas: "+matrizSeries[i][1]+" | Capítulos: "+matrizSeries[i][2]+" | Temporada Actual: "+matrizSeries[i][3]+" | Capítulo Actual (de temporada): "+matrizSeries[i][4]+" | Rating: "+matrizSeries[i][5]+" | Comentarios: "+matrizSeries[i][6]);
+               System.out.println("『 Título: "+matrizSeries[i][0]+" | Temporadas: "+matrizSeries[i][1]+" | Capítulos: "+matrizSeries[i][2]+" | Temporada Actual: "+matrizSeries[i][3]+" | Capítulo Actual (de temporada): "+matrizSeries[i][4]+" | Estado: "+matrizSeries[i][5]+" | Rating: "+matrizSeries[i][6]+" | Comentarios: "+matrizSeries[i][7]+" 』");
            }
         }
     }
@@ -132,7 +232,7 @@ public class chillManager {
         System.out.print("Ingrese el número de la opción que desea seleccionar: ");
     }
 
-    //Función de leer la opción del menú.
+    //Función de leer la opción del menú dentro del rango de operaciones disponible.
     public static int leerOpcion(){
         int opcion;
         while (true) {
@@ -143,7 +243,7 @@ public class chillManager {
                 } else {
                     System.out.print("Opción inválida. Inténtelo nuevamente: ");
                 }
-            } catch(Exception InputMismatchException){
+            } catch(Exception InputMismatchException) {
                 System.out.print("Entrada no válida. Ingrese un número: ");
             }
         }
@@ -153,23 +253,38 @@ public class chillManager {
     //Función de ejecutar las opciones del menú.
     public static void ejecutarOpcion(Object[][] matrizSeries, int opcion){
         if (opcion == 1) { //agregar
+            agregarSerie(matrizSeries,pedirString("Ingrese el título de la serie: "));
         } else if (opcion == 2) { //buscar serie.
+            buscarSerie(matrizSeries,pedirString("Ingrese el título de la serie: "));
         } else if (opcion == 3) { //actualizar serie.
+            actualizarSerie(matrizSeries,pedirString("Ingrese el título de la serie: "));
         } else if (opcion == 4) { //eliminar serie.
-        } else if (opcion == 5) { //ver todos serie.
-        } else if (opcion == 6) { //salir serie.
+            eliminarSerie(matrizSeries,pedirString("Ingrese el título de la serie: "));
+        } else if (opcion == 5) { //ver todas las series.
+            mostrarSeries(matrizSeries);
+        } else if (opcion == 6) { //salir del menú.
             System.out.print("A continuación, confirme. "); //se ejecutará el continuar en el menú.
         }
     }
 
     //Función booleana de continuar para el menú.
-    public static boolean continuarMenu(int opcion){
+    public static boolean continuarMenu(){
         boolean continuar = true;
-        if(opcion == 2){
-            continuar = false;
-            System.out.println("Cerrando menú... ¡Hasta luego! ^^");
-        } else if(opcion != 1){
-            System.out.println("Opción no válida.");
+        int opcion;
+        while (true) {
+            try {
+                opcion = scanner().nextInt();
+                if (opcion == 1) {
+                    break;
+                } else if (opcion == 2) {
+                    continuar = false;
+                    break;
+                } else {
+                    System.out.print("Opción inválida. Inténtelo nuevamente: ");
+                }
+            } catch (Exception InputMismatchException) {
+                System.out.print("Entrada no válida. Ingrese un número: ");
+            }
         }
         return continuar;
     }
@@ -187,10 +302,9 @@ public class chillManager {
             int opcion = leerOpcion();
             ejecutarOpcion(matrizSeries, opcion);
             System.out.print("¿Desea realizar otra operación? (1 = Sí ; 2 = No): ");
-            int respuesta = scanner().nextInt();
-            continuar = continuarMenu(respuesta);
+            continuar = continuarMenu();
         }
+        System.out.println("Cerrando menú... ¡Hasta luego! ^^");
         scanner().close();
     }
-
 }
