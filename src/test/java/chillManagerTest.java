16e1bb9da9e39ecc422dd2688761b3ce8913f41e
martingci <m.carrasco29@ufromail.cl>
2024-09-15 21:31:21 -0300

    Se realizan test de métodos básicos para comprobar la funcionalidad. Específicamente las funciones de conseguir un número de fila, comprobar si es el juego único y comprobación de espacio disponible.

---
+++ src/test/java/chillManagerTest.java
@@ -0,0 +1,53 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class chillManagerTest {
+
+    Object [][] matrizJuegos = new Object[5][7];
+
+    @BeforeEach
+    void setUp() {
+        matrizJuegos[0] = new Object[]{"Serie1",1,1,1,"Sin empezar", 10, "Comentario"};
+        matrizJuegos[1] = new Object[]{"Serie2",1,1,1,"Sin empezar", 10, "Comentario"};
+        matrizJuegos[2] = new Object[]{"Serie3",1,1,1,"Sin empezar", 10, "Comentario"};
+        matrizJuegos[3] = new Object[]{"Serie4",1,1,1,"Sin empezar", 10, "Comentario"};
+        matrizJuegos[4] = new Object[]{"Serie5",1,1,1,"Sin empezar", 10, "Comentario"};
+    }
+
+    @AfterEach
+    void tearDown() {
+        matrizJuegos[0] = null;
+        matrizJuegos[1] = null;
+        matrizJuegos[2] = null;
+        matrizJuegos[3] = null;
+        matrizJuegos[4] = null;
+    }
+    @Test
+    void conseguirNumeroFila() {
+        assertEquals(0,chillManager.obtenerFilaJuego(matrizJuegos,"Serie1"));
+        assertEquals(1,chillManager.obtenerFilaJuego(matrizJuegos,"Serie2"));
+        assertEquals(2,chillManager.obtenerFilaJuego(matrizJuegos,"Serie3"));
+        assertEquals(3,chillManager.obtenerFilaJuego(matrizJuegos,"Serie4"));
+        assertEquals(4,chillManager.obtenerFilaJuego(matrizJuegos,"Serie5"));
+    }
+    @Test
+    void juegoUnica () {
+        assertFalse(chillManager.juegoUnico(matrizJuegos,"Serie1"));
+        assertFalse(chillManager.juegoUnico(matrizJuegos,"Serie2"));
+        assertFalse(chillManager.juegoUnico(matrizJuegos,"Serie3"));
+        assertFalse(chillManager.juegoUnico(matrizJuegos,"Serie4"));
+        assertFalse(chillManager.juegoUnico(matrizJuegos,"Serie5"));
+        assertTrue(chillManager.juegoUnico(matrizJuegos,"Serie6"));
+    }
+    @Test
+    void espaciodisponible () {
+        assertFalse(chillManager.freeSpace(matrizJuegos));
+        chillManager.hacerFilaNull(matrizJuegos,0);
+        assertTrue(chillManager.freeSpace(matrizJuegos));
+        chillManager.agregarJuego(matrizJuegos,"titulo10", "No empezado", 2002, 0, 10, "");
+        assertFalse(chillManager.freeSpace(matrizJuegos));
+    }
+}
