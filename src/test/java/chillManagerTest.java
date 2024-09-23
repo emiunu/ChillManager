61e8301c0983663d286ec4209ad7cdc201ab5dcb
emiunu <e.jaramillo02@ufromail.cl>
2024-09-23 00:14:20 -0300

    Arreglo de test

--- src/test/java/chillManagerTest.java
+++ src/test/java/chillManagerTest.java
@@ -8,17 +8,17 @@ class chillManagerTest {
     Object[][] matrizPeliculas = chillManager.crearMatrizPeliculas();
     @BeforeEach
     void setUp(){
-            chillManager.annadirPelicula(matrizPeliculas,"El cisne negro",2011,"Thriller", 108,"Completado.",10,"Muy buena");
-            chillManager.annadirPelicula(matrizPeliculas,"Pearl",2022,"Thriller",102, "Completado.",10,"Muy buena");
-            chillManager.annadirPelicula(matrizPeliculas,"Cars",2006,"Comedia",117,"Sin ver.",1,"");
-            chillManager.annadirPelicula(matrizPeliculas,"WALL E", 2008,"Ciencia ficción",98,"Sin ver",1,"");
-            chillManager.annadirPelicula(matrizPeliculas,"Terminator",1984,"Ciencia ficción",107,"Sin ver",1,"");
+            chillManager.agregarPelicula(matrizPeliculas,"El cisne negro",2011,"Thriller", 108,"Completado.",10,"Muy buena");
+            chillManager.agregarPelicula(matrizPeliculas,"Pearl",2022,"Thriller",102, "Completado.",10,"Muy buena");
+            chillManager.agregarPelicula(matrizPeliculas,"Cars",2006,"Comedia",117,"Sin ver.",1,"");
+            chillManager.agregarPelicula(matrizPeliculas,"WALL E", 2008,"Ciencia ficción",98,"Sin ver",1,"");
+            chillManager.agregarPelicula(matrizPeliculas,"Terminator",1984,"Ciencia ficción",107,"Sin ver",1,"");
     }
 
     @Test
     void conseguirNumeroFila() {
-        assertEquals(1,chillManager.conseguirNumeroFila(matrizPeliculas,"Pearl"));
-        assertEquals(2,chillManager.conseguirNumeroFila(matrizPeliculas,"Cars"));
+        assertEquals(1,chillManager.obtenerFilaPelicula(matrizPeliculas,"Pearl"));
+        assertEquals(2,chillManager.obtenerFilaPelicula(matrizPeliculas,"Cars"));
     }
 
     @Test
@@ -32,11 +32,11 @@ class chillManagerTest {
     @Test
     void espacioDisponible(){
         assertTrue(chillManager.espacioDisponible(matrizPeliculas));
-        chillManager.annadirPelicula(matrizPeliculas,"Spiderverse",2023,"Acción",140,"Sin ver",1,"");
-        chillManager.annadirPelicula(matrizPeliculas,"Terminator 2",1991,"Ciencia ficción",157,"Sin ver",1,"");
-        chillManager.annadirPelicula(matrizPeliculas,"Terminator 3",2003,"Ciencia ficción",109,"Sin ver",1,"");
-        chillManager.annadirPelicula(matrizPeliculas,"Up",2009,"Aventura",96,"Sin ver",1,"");
-        chillManager.annadirPelicula(matrizPeliculas,"Matrix",1999,"Ciencia ficción",136,"Sin ver",1,"");
+        chillManager.agregarPelicula(matrizPeliculas,"Spiderverse",2023,"Acción",140,"Sin ver",1,"");
+        chillManager.agregarPelicula(matrizPeliculas,"Terminator 2",1991,"Ciencia ficción",157,"Sin ver",1,"");
+        chillManager.agregarPelicula(matrizPeliculas,"Terminator 3",2003,"Ciencia ficción",109,"Sin ver",1,"");
+        chillManager.agregarPelicula(matrizPeliculas,"Up",2009,"Aventura",96,"Sin ver",1,"");
+        chillManager.agregarPelicula(matrizPeliculas,"Matrix",1999,"Ciencia ficción",136,"Sin ver",1,"");
         assertFalse(chillManager.espacioDisponible(matrizPeliculas));
     }
 }
