e0f25a1129d155f2474c88934cf0d8ae15ebbf0d
emiunu <e.jaramillo02@ufromail.cl>
2024-09-22 22:51:42 -0300

    Arreglo de funciones de peliculas y compatibilidad con las otras ramas

--- src/main/java/chillManager.java
+++ src/main/java/chillManager.java
@@ -1,10 +1,9 @@
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class chillManager {
 
     public static void main(String[] args) {
-        menu();
+        menuPeliculas(crearMatrizPeliculas());
     }
 
     // Función para crear matriz de pelicula
@@ -13,7 +12,7 @@ public class chillManager {
     }
 
     //Función para añadir valores a la matriz pelicula
-    public static void annadirPelicula(Object[][] matrizPeliculas, String titulo, int anno, String genero, int duracion, String status,int rating, String comentario){
+    public static void agregarPelicula(Object[][] matrizPeliculas, String titulo, int anno, String genero, int duracion, String status,int rating, String comentario){
         for (int i = 0; i < matrizPeliculas.length; i++) {
             if (matrizPeliculas[i][0] == null || matrizPeliculas[i][0] == "" ) {
                 matrizPeliculas[i][0] = titulo;
@@ -30,71 +29,60 @@ public class chillManager {
     }
 
     //Funcion para ejecutar añadir películas
-    public static void ejecutarAnnadirPelicula(Object [][] matrizPeliculas){
+    public static void ejecutarAgregarPelicula(Object [][] matrizPeliculas){
         if (espacioDisponible(matrizPeliculas)){
-            System.out.print("Ingrese el titulo de la película: ");
-            String titulo = ingresarString();
+            String titulo = pedirString("Ingrese el titulo de la película que quiere agregar: ");
             boolean peliculaUnica = peliculaUnica(matrizPeliculas,titulo);
             if (peliculaUnica){
-                System.out.print("Ingrese el año de la película: ");
-                int anno = ingresarInt();
-                System.out.print("Ingrese el genero de la película: ");
-                String genero = ingresarString();
-                System.out.print("Ingrese la duración de la película: ");
-                int duracion = ingresarInt();
-                String status = ingresarStatus();
-                int rating = ingresarRating();
-                String comentario = ingresarComentario();
-                annadirPelicula(matrizPeliculas,titulo,anno,genero,duracion,status,rating,comentario);
+                agregarPelicula(matrizPeliculas,titulo,pedirIntPositivo("Ingrese el año de la película: "),pedirString("Ingrese el género de la película: "),pedirIntPositivo("Ingrese la duración de la película: "),ingresarStatusPelicula(),obtenerRating(),pedirString("Ingrese un comentario: "));
                 System.out.println("Película guardada correctamente.");
             } else {
-                System.out.println("Esta película ya se encuentra guardada.");
+                System.out.println("La película ya se encuentra registrada.");
             }
         } else {
-            System.out.println("No hay espacio disponible en la matriz :(.");
+            System.out.println("No queda espacio disponible para agregar películas.");
         }
     }
 
     //Funcion para visualizar datos guardados en la matriz
     public static void verMatrizPeliculas(Object[][] matrizPeliculas){
-        System.out.println("Listado de peliculas: ");
+        System.out.println("LISTA DE PELÍCULAS");
+        System.out.println("===================================");
         for (int i = 0; i < matrizPeliculas.length; i++) {
-            if (matrizPeliculas[i][0] == null) {
-                continue;
-            }else{
-                System.out.println("Pelicula: " + matrizPeliculas[i][0] + "| Año: " + matrizPeliculas[i][1] + "| Género: " + matrizPeliculas[i][2] + "| Duración: " + matrizPeliculas[i][3] + "| Estado: " + matrizPeliculas[i][4]+ "| Valoracion: " + matrizPeliculas[i][5] + "| Comentario: " + matrizPeliculas[i][6]);
-
+            if (matrizPeliculas[i][0] != null) {
+                System.out.println("『 Título: "+matrizPeliculas[i][0]+" | Año: "+matrizPeliculas[i][1]+" | Género: "+matrizPeliculas[i][2]+" | Duracion: "+matrizPeliculas[i][3]+" | Estado: "+matrizPeliculas[i][4]+" | Rating: "+matrizPeliculas[i][5]+" | Comentarios: "+matrizPeliculas[i][6]+" 』");
             }
         }
-        System.out.println("Fin del listado.");
+        System.out.println("FIN DEL LISTADO.");
+
     }
 
     //Función para eliminar peliculas de la matriz
     public static void eliminarPelicula(Object [][] matrizPeliculas, String titulo){
         for (int i = 0; i < matrizPeliculas.length; i++) {
             if (matrizPeliculas[i][0] != null && matrizPeliculas[i][0].equals(titulo)) {
-                matrizPeliculas[i][0] = null;
-                matrizPeliculas[i][1] = null;
-                matrizPeliculas[i][2] = null;
-                matrizPeliculas[i][3] = null;
-                matrizPeliculas[i][4] = null;
-                matrizPeliculas[i][5] = null;
-                matrizPeliculas[i][6] = null;
+                vaciarFila(matrizPeliculas,i,7);
                 break;
             }
         }
     }
 
+    public static Object[][] vaciarFila(Object [][] matriz, int fila, int columnas){
+        for (int i = 0; i < columnas; i++) {
+            matriz[fila][i] = null;
+        }
+        return matriz;
+    }
+
     //Función para ejecutar eliminar película
     public static void ejecutarEliminarPelicula(Object[][] matrizPeliculas){
-        System.out.print("Ingrese el titulo de la película que quiere eliminar: ");
-        String titulo = ingresarString();
+        String titulo = pedirString("Ingrese el titulo de la película que quiere eliminar: ");
         boolean peliculaUnica = peliculaUnica(matrizPeliculas,titulo);
         if (!peliculaUnica){
             eliminarPelicula(matrizPeliculas,titulo);
-            System.out.println("Pelicula eliminada corrrectamente.");
+            System.out.println("Película eliminada correctamente.");
         } else {
-            System.out.println("Esta película no se encuentra guardada.");
+            System.out.println("No se encontró la película");
         }
     }
 
@@ -102,7 +90,7 @@ public class chillManager {
     public static void buscarPelicula(Object [][] matrizPeliculas, String titulo){
         for (int i = 0; i < matrizPeliculas.length; i++) {
             if ( matrizPeliculas[i][0] != null && matrizPeliculas[i][0].equals(titulo)) {
-                System.out.println("Pelicula: " + matrizPeliculas[i][0] + "| Año: " + matrizPeliculas[i][1] + "| Género: " + matrizPeliculas[i][2] + "| Duración: " + matrizPeliculas[i][3] + "| Estado: " + matrizPeliculas[i][4]+ "| Valoracion: " + matrizPeliculas[i][5] + "| Comentario: " + matrizPeliculas[i][6]);
+                System.out.println("『 Título: "+matrizPeliculas[i][0]+" | Año: "+matrizPeliculas[i][1]+" | Género: "+matrizPeliculas[i][2]+" | Duracion: "+matrizPeliculas[i][3]+" | Estado: "+matrizPeliculas[i][4]+" | Rating: "+matrizPeliculas[i][5]+" | Comentarios: "+matrizPeliculas[i][6]+" 』");
                 break;
             }
         }
@@ -110,38 +98,24 @@ public class chillManager {
 
     //Función para ejecutar buscar película
     public static void ejecutarBuscarPelicula(Object [][] matrizPeliculas){
-        System.out.print("Ingresa el titulo de la película que quieres buscar: ");
-        String titulo = ingresarString();
+        String titulo = pedirString("Ingresa el titulo de la película que quiere buscar: ");
         boolean peliculaUnica = peliculaUnica(matrizPeliculas,titulo);
         if (!peliculaUnica){
-            System.out.println("Resultado de busqueda:");
+            System.out.println("Película encontrada.");
             buscarPelicula(matrizPeliculas,titulo);
         } else {
-            System.out.println("La pelicula no se encuentre guardada.");
+            System.out.println("No se encontró la película.");
         }
     }
 
     //Función para actualizar películas de la matriz
     public static void ejecutarActualizarPelicula(Object[][] matrizPeliculas){
-        System.out.print("Ingrese el titulo de la película que quieres actualizar: ");
-        String titulo = ingresarString();
+        String titulo = pedirString("Ingrese el titulo de la película que quiere actualizar/modificar: ");
         boolean peliculaUnica = peliculaUnica(matrizPeliculas,titulo);
         if (!peliculaUnica){
-            int fila = conseguirNumeroFila(matrizPeliculas, titulo);
-            System.out.print("Ingrese el titulo: ");
-            String nuevoTitulo = ingresarString();
-            System.out.print("Ingrese el año: ");
-            int nuevoAnno = ingresarInt();
-            System.out.print("Ingrese el genero: ");
-            String nuevoGenero = ingresarString();
-            System.out.print("Ingrese la duración: ");
-            int nuevaDuracion = ingresarInt();
-            String nuevoStatus = ingresarStatus();
-            int nuevoRating = ingresarRating();
-            String nuevoComentario = ingresarComentario();
-            actualizarPelicula(matrizPeliculas,fila,nuevoTitulo,nuevoAnno,nuevoGenero,nuevaDuracion,nuevoStatus,nuevoRating,nuevoComentario);
+            actualizarPelicula(matrizPeliculas,obtenerFilaPelicula(matrizPeliculas, titulo),pedirString("Ingrese el nuevo titulo: "),pedirIntPositivo("Ingrese el año: "),pedirString("Ingrese el género: "),pedirIntPositivo("Ingrese la duración: "),ingresarStatusPelicula(),obtenerRating(),pedirString("Ingrese comentario: "));
         } else {
-            System.out.println("La película no se encuentra guardada.");
+            System.out.println("No se encontró la película.");
         }
 
     }
@@ -159,7 +133,7 @@ public class chillManager {
     }
 
     //Función para conseguir numero de fila
-    public static int conseguirNumeroFila(Object[][] matrizPeliculas, String titulo){
+    public static int obtenerFilaPelicula(Object[][] matrizPeliculas, String titulo){
         int fila = 0;
         for (int i = 0; i < matrizPeliculas.length; i++) {
             if(matrizPeliculas[i][0] != null && matrizPeliculas[i][0].equals(titulo)) {
@@ -189,7 +163,8 @@ public class chillManager {
     }
 
     //Función para scanner de texto; título, y género.
-    public static String ingresarString(){
+    public static String pedirString(String mensaje){
+        System.out.print(mensaje);
         String string;
         while (true){
             string = scanner().nextLine();
@@ -203,47 +178,44 @@ public class chillManager {
     }
 
     //Función para ingresar int que sean números enteros positivos
-    public static int ingresarInt() {
-        int intIngresar;
+    public static int pedirInt(String mensaje){
+        int valor;
         while (true) {
             try {
-                intIngresar = scanner().nextInt();
-                if (intIngresar > 0) {
-                    break;
-                } else {
-                    System.out.print("El numero ingresado no es valido. Intente de nuevo: ");
-                }
-
-
-            } catch (InputMismatchException e) {
-                System.out.print("Entrada no valida. Ingrese un numero: ");
+                System.out.print(mensaje);
+                valor = scanner().nextInt();
+                break;
+            } catch(Exception InputMismatchException) {
+                System.out.println("Entrada no válida. Ingrese un número. ");
             }
         }
-        return intIngresar;
+        return valor;
+    }
+
+    public static int pedirIntPositivo (String mensaje) {
+        while (true) {
+            int numero = pedirInt(mensaje);
+            if (numero < 0) {
+                System.out.println("Ingrese un número positivo.");
+            } else {
+                return numero;
+            }
+        }
+    }
+
+    public static int obtenerRating(){
+        return leerOpcionLimitada("Ingrese su rating (del 1 al 10; 1 = puntaje más bajo ; 10 puntaje más alto): ", 1, 10);
     }
 
     //Funcion para ingresar Status
-    public static String ingresarStatus(){
-        String status;
-        int opcion;
-        while(true){
-            try{
-                opcionesStatusPelicula();
-                opcion = scanner().nextInt();
-                if (opcion == 1){
-                    status = "Sin ver.";
-                    break;
-                } else if (opcion == 2){
-                    status = "Completado.";
-                    break;
-                }else{
-                    System.out.println("El numero ingresado no es valido. Intente de nuevo. ");
-
-                }
-
-            } catch (InputMismatchException e){
-                System.out.println("Entrada no valida. Ingrese un numero: ");
-            }
+    public static String ingresarStatusPelicula(){
+        String status = "";
+        opcionesStatusPelicula();
+        int opcion = leerOpcionLimitada("Seleccione una opción: ",1, 2);
+        if (opcion == 1){
+            status = "Sin ver";
+        }else {
+            status = "Completado";
         }
         return status;
     }
@@ -251,36 +223,11 @@ public class chillManager {
     //Función que muestra las opciones de status
     public static void opcionesStatusPelicula(){
         System.out.println("Status de película:");
-        System.out.println("1. Sin ver");
-        System.out.println("2. Completado");
-        System.out.print("Seleccione una opción: ");
+        System.out.println("1. Sin ver.");
+        System.out.println("2. Completado.");
     }
 
-    //Función para ingresar rating
-    public static int ingresarRating(){
-        int rating;
-        while(true){
-            try{
-                System.out.print("Ingrese el rating (1 al 10): ");
-                rating = scanner().nextInt();
-                if (rating >= 1 && rating <=10){
-                    break;
-                } else {
-                    System.out.println("El numero ingresado no es valido. Intente de nuevo. ");
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("Entrada no valida. Ingrese un numero: ");
-            }
-        }
-        return rating;
-    }
 
-    public static String ingresarComentario(){
-        String comentario;
-        System.out.print("Ingrese el comentario a la película: ");
-        comentario = scanner().nextLine();
-        return comentario;
-    }
 
     //Función para saber si hay espacio disponible, esto se utilizará cuando se quiera agregar mas alla del length de la matriz Peliculas
     public static boolean espacioDisponible(Object [][] matrizPeliculas){
@@ -293,30 +240,29 @@ public class chillManager {
     }
 
     //Función imprimir opciones del menú.
-    public static void mostrarOpciones(){
-        System.out.println("\nMenú de Peliculas:");
-        System.out.println("1) Agregar pelicula.");
-        System.out.println("2) Buscar pelicula.");
-        System.out.println("3) Actualizar/modificar pelicula.");
-        System.out.println("4) Eliminar pelicula.");
-        System.out.println("5) Ver todas las peliculas.");
-        System.out.println("6) Salir.");
-        System.out.print("Ingrese el número de la opción que desea seleccionar: ");
+    public static void mostrarOpcionesPeliculas(){
+        System.out.println("\nMenú de Películas:");
+        System.out.println("1) Agregar película.");
+        System.out.println("2) Buscar película.");
+        System.out.println("3) Actualizar/modificar película.");
+        System.out.println("4) Eliminar película.");
+        System.out.println("5) Ver todas las películas.");
+        System.out.println("6) Volver.");
     }
 
 
-    //Función de leer la opción del menú.
-    public static int leerOpcion(){
+    public static int leerOpcionLimitada(String mensaje, int min, int max){
+        System.out.print(mensaje);
         int opcion;
         while (true) {
             try {
                 opcion = scanner().nextInt();
-                if ((opcion >= 1) && (opcion <= 6)) {
+                if ((opcion >= min) && (opcion <= max)) {
                     break;
                 } else {
-                    System.out.print("Opción inválida. Inténtelo nuevamente: ");
+                    System.out.print("Opción sale del rango. Inténtelo nuevamente: ");
                 }
-            } catch(InputMismatchException e ){
+            } catch(Exception InputMismatchException){
                 System.out.print("Entrada no válida. Ingrese un número: ");
             }
         }
@@ -326,9 +272,9 @@ public class chillManager {
 
 
     //Función de ejecutar las opciones del menú.
-    public static void ejecutarOpcion(Object[][] matrizPeliculas, int opcion){
+    public static void ejecutarOpcionPeliculas(Object[][] matrizPeliculas, int opcion){
         if (opcion == 1) { //añadir
-            ejecutarAnnadirPelicula(matrizPeliculas);
+            ejecutarAgregarPelicula(matrizPeliculas);
         } else if (opcion == 2) { //buscar.
             ejecutarBuscarPelicula(matrizPeliculas);
         } else if (opcion == 3) { //actualizar.
@@ -337,49 +283,21 @@ public class chillManager {
             ejecutarEliminarPelicula(matrizPeliculas);
         } else if (opcion == 5) { //ver todos.
             verMatrizPeliculas(matrizPeliculas);
-        } else if (opcion == 6) { //salir.
-            System.out.print("A continuación, confirme. "); //se ejecutará el continuar en el menú.
+        } else if (opcion == 6) { //volver al menú.
+            System.out.print("Volviendo al menú general...");
         }
     }
-    //Función booleana de continuar para el menú.
-    public static boolean continuarMenu(){
-        int opcion;
-        boolean continuar;
-        while (true){
-            try {
-                opcion = scanner().nextInt();
-                if(opcion == 2){
-                    continuar = false;
-                    System.out.println("Cerrando menú... ¡Hasta luego!");
-                    break;
-                } else if(opcion == 1){
-                    continuar = true;
-                    break;
-                } else {
-                    System.out.print("Ingrese una opción valida: ");
-                }
-            } catch (InputMismatchException e){
-                System.out.print("Entrada no valida. Ingrese un numero: ");
-            }
-
-        }
-
-        return continuar;
-    }
-
 
     //Función menú.
-    public static void menu(){
-        boolean continuar = true;
-        Object [][] matrizPeliculas = crearMatrizPeliculas();
-        while(continuar){
-            mostrarOpciones();
-            int opcion = leerOpcion();
-            ejecutarOpcion(matrizPeliculas, opcion);
-            System.out.print("¿Desea realizar otra operación? (1 = Sí ; 2 = No): ");
-            continuar = continuarMenu();
+    public static void menuPeliculas(Object [][] matrizPeliculas){
+        while(true){
+            mostrarOpcionesPeliculas();
+            int opcion = leerOpcionLimitada("Ingrese el número de la opción que desea seleccionar: ",1,6);
+            ejecutarOpcionPeliculas(matrizPeliculas, opcion);
+            if (opcion == 6){
+                break;
+            }
         }
-        scanner().close();
     }
 
 }
