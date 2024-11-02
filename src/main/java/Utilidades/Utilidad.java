61e794b9d3f81fbeaa240748be94efad9fdeca31
emiunu <e.jaramillo02@ufromail.cl>
2024-11-02 17:57:42 -0300

    Se agregan las funciones de Utilidad

--- src/main/java/Utilidades/Utilidad.java
+++ src/main/java/Utilidades/Utilidad.java
@@ -9,8 +9,15 @@ public class Utilidad {
 	 * @param mensaje
 	 */
 	public String pedirString(String mensaje) {
-		// TODO - implement Utilidad.pedirString
-		throw new UnsupportedOperationException();
+		System.out.print(mensaje);
+		String string;
+		while (true){
+			string = scanner().nextLine();
+			if (string.isEmpty()){
+				System.out.print("Por favor ingrese una entrada no vacía: ");
+			}else{break;}
+		}
+		return string;
 	}
 
 	/**
@@ -18,8 +25,17 @@ public class Utilidad {
 	 * @param mensaje
 	 */
 	public int pedirInt(String mensaje) {
-		// TODO - implement Utilidad.pedirInt
-		throw new UnsupportedOperationException();
+		int value;
+		while (true) {
+			try {
+				System.out.print(mensaje);
+				value = scanner().nextInt();
+				break;
+			} catch(Exception InputMismatchException){
+				System.out.print("Entrada no válida. Ingrese un número. "); // excepcion en caso de no ser numero
+			}
+		}
+		return value;
 	}
 
 	/**
@@ -29,13 +45,25 @@ public class Utilidad {
 	 * @param max
 	 */
 	public int leerOpcionLimitada(String mensaje, int min, int max) {
-		// TODO - implement Utilidad.leerOpcionLimitada
-		throw new UnsupportedOperationException();
+		System.out.print(mensaje);
+		int opcion;
+		while (true) {
+			try {
+				opcion = scanner().nextInt();
+				if ((opcion >= min) && (opcion <= max)) {
+					break;
+				} else {
+					System.out.print("Opción sale del rango. Inténtelo nuevamente: ");
+				}
+			} catch(Exception InputMismatchException){
+				System.out.print("Entrada no válida. Ingrese un número: ");
+			}
+		}
+		return opcion;
 	}
 
 	public Scanner scanner() {
-		// TODO - implement Utilidad.scanner
-		throw new UnsupportedOperationException();
+		return new Scanner(System.in);
 	}
 
 	/**
@@ -43,8 +71,14 @@ public class Utilidad {
 	 * @param mensaje
 	 */
 	public int pedirIntPositivo(String mensaje) {
-		// TODO - implement Utilidad.pedirIntPositivo
-		throw new UnsupportedOperationException();
+		while (true) {
+			int numero = pedirInt(mensaje);
+			if (numero < 0) {
+				System.out.println("Ingrese un número positivo.");
+			} else {
+				return numero;
+			}
+		}
 	}
 
 }
