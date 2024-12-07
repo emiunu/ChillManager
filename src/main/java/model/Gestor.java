57907d17e32d770f6b07ae2e9cfcf24f91a2c82e
martingci <m.carrasco29@ufromail.cl>
2024-12-06 21:23:03 -0300

    Se modifica de vector a ArrayList.

--- src/main/java/model/Gestor.java
+++ src/main/java/model/Gestor.java
@@ -1,8 +1,9 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.Vector;
 import model.Actividad;
 
 public class Gestor {
-	private Vector<Actividad> actividades = new Vector<Actividad>();
+	private ArrayList<Actividad> actividades;
 }
