e79a6315ced76f264a5b456aa53ffd65a1659133
Sabrina <s.lopez15@ufromail.cl>
2025-01-04 20:33:36 -0300

    Se sube main para pruebas y se eliminan carpetas de avance anterior.

---
+++ src/main/java/principal/Main.java
@@ -0,0 +1,28 @@
+package principal;
+
+import model.*;
+import controller.*;
+import guis.*;
+
+public class Main {
+    public static void main(String[] args) {
+        run();
+    }
+
+    public static void run(){
+        Gestor ChillManager = new Gestor();
+        Juego juego1 = new Juego("Juego 1",2024,1,Estado.EN_PROGRESO,10,"WAA");
+        Libro libro1 = new Libro(1234567890,"Libro 1","Autor",2024,Estado.EN_PROGRESO,0,"waa");
+        Pelicula pelicula1 = new Pelicula("Pelicula 1",2024,120,Estado.EN_PROGRESO,10,"wa a");
+        Serie serie1 = new Serie("Serie 1",2,20,1,10,Estado.EN_PROGRESO,10,"WaA");
+        ChillManager.agregarActividad(juego1);
+        ChillManager.agregarActividad(libro1);
+        ChillManager.agregarActividad(pelicula1);
+        ChillManager.agregarActividad(serie1);
+        Controlador.guardarDatos(ChillManager);
+        Controlador.cargarDatos(ChillManager);
+
+        VentanaPrincipal ventanaPrincipal = new VentanaPrincipal(ChillManager);
+        ventanaPrincipal.setVisible(true);
+    }
+}
