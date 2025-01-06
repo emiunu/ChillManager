9b69c96b0f7f3c2dc0d9d30eae0bb9391ebac499
defocuss <d.baez03@ufromail.cl>
2025-01-06 20:44:26 -0300

    Se crea javadoc para Utilidad.

--- src/main/java/utils/Utilidad.java
+++ src/main/java/utils/Utilidad.java
@@ -3,12 +3,29 @@ import model.*;
 
 import java.util.Objects;
 
+/**
+ * Clase Utilidad que se encarga de manejar los metodos basicos
+ * Usados en el programa.
+ */
+
+
 public class Utilidad {
 
+    /**
+     * Método que elimina los espacios y convierte una cadena a minúsculas.
+     * @param cadena La cadena a trabajar.
+     * @return La cadena sin espacios y en minúsculas.
+     */
     public static String minusSinEspacio(String cadena) {
         return cadena.replace(" ","").toLowerCase();
     }
 
+    /**
+     * Método que verifica si un título es único.
+     * @param buscando El título a buscar.
+     * @param gestor El gestor de actividades.
+     * @return True si el título es único, false si no.
+     */
     public static boolean tituloUnico(String buscando, Gestor gestor){
         for (Actividad actividadEnLista : gestor.getActividades()) {
             if (Objects.equals(minusSinEspacio(buscando), minusSinEspacio(actividadEnLista.getTitulo()))) {
@@ -18,6 +35,12 @@ public class Utilidad {
         return true;
     }
 
+    /**
+     * Método que entrega una actividad del gestor según su título.
+     * @param buscando El título de la actividad a buscar.
+     * @param gestor El gestor de actividades.
+     * @return La actividad encontrada o null si no se encuentra.
+     */
     public static Actividad entregarActividad(String buscando, Gestor gestor){
         for (Actividad actividadEnLista : gestor.getActividades()) {
             if (Objects.equals(minusSinEspacio(buscando), minusSinEspacio(actividadEnLista.getTitulo()))) {
@@ -27,11 +50,17 @@ public class Utilidad {
         return null;
     }
 
+    /**
+     * Método que verifica si un ISBN es único en el gestor.
+     * @param buscando El ISBN a buscar.
+     * @param gestor El gestor de actividades.
+     * @return True si el ISBN es único, false si no.
+     */
     public static boolean isbnUnico(int buscando, Gestor gestor) { //int o long?
         for (Actividad actividadEnLista : gestor.getActividades()) {
             if (Objects.equals(actividadEnLista.getTipo(), "Libro")) {
                 Libro libro = (Libro) actividadEnLista;
-                if (libro.getIsbn()==buscando) {
+                if (libro.getIsbn() == buscando) {
                     return false;
                 }
             }
@@ -39,6 +68,11 @@ public class Utilidad {
         return true;
     }
 
+    /**
+     * Método que verifica si una cadena está vacía.
+     * @param cadena La cadena a verificar.
+     * @return True si la cadena está vacía, false si no.
+     */
     public static boolean stringVacio(String cadena) {
         return Objects.equals(minusSinEspacio(cadena), "");
     }
